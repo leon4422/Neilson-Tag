099763399567d61022562261f463fd4dee7acf5a
Leon Augustine <leon_augustine@hotmail.com>
2011-09-14 10:42:01 -0400

    returns HTML

--- NielsenTag/NielsenTag/Program.cs
+++ NielsenTag/NielsenTag/Program.cs
@@ -1,16 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
 
-namespace NielsenTag
+
+/// <summary>
+/// Fetches a Web Page
+/// </summary>
+class WebFetch
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // used to build entire input
+        StringBuilder sb = new StringBuilder();
+
+        // used on each read operation
+        byte[] buf = new byte[8192];
+
+        // prepare the web page we will be asking for
+        HttpWebRequest request = (HttpWebRequest)
+            WebRequest.Create("http://www.csee.usf.edu/~christen/class6/class6.html");
+
+        // execute the request
+        HttpWebResponse response = (HttpWebResponse)
+            request.GetResponse();
+
+        // we will read data via the response stream
+        Stream resStream = response.GetResponseStream();
+
+        string tempString = null;
+        int count = 0;
+
+        do
         {
-            Console.Write("Hello World!");
-            Console.ReadKey(true);
+            // fill the buffer with data
+            count = resStream.Read(buf, 0, buf.Length);
+
+            // make sure we read some data
+            if (count != 0)
+            {
+                // translate from bytes to ASCII text
+                tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                // continue building the string
+                sb.Append(tempString);
+            }
         }
+        while (count > 0); // any more data to read?
+
+        // print out page source
+        Console.WriteLine(sb.ToString());
+        Console.ReadLine();
     }
 }
